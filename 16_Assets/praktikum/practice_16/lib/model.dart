744a7336decfba38fce8ba8ba8d492cea6dbf9e9
Madd-G <akhmadnuralamsyah@gmail.com>
2022-09-24 08:58:15 +0700

    added practice 16

---
+++ 16_Assets/praktikum/practice_16/lib/model.dart
@@ -0,0 +1,62 @@
+class Img {
+  int id;
+  String name;
+  String url;
+
+  Img({
+    required this.id,
+    required this.name,
+    required this.url,
+  });
+}
+
+List<Img> image = [
+  Img(
+    id: 1,
+    name: 'Lion',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN4xL9y_H7z21gIpe2CUi6Yi9og-8--7JUaQ&usqp=CAU',
+  ),
+  Img(
+    id: 2,
+    name: 'Puma',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxKKedhC8dVN204A7wl9x2n315MM-lF3EmuA&usqp=CAU',
+  ),
+  Img(
+    id: 3,
+    name: 'Goat',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCw0HwALk4jUZtSPHRaswNJEQ5YvWqrAvCIg&usqp=CAU',
+  ),
+  Img(
+    id: 4,
+    name: 'Cat',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLliy_lUMzOFFvxRihBMwas458el-v3g6U5Q&usqp=CAU',
+  ),
+  Img(
+    id: 5,
+    name: 'Eagle',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx8x8cq3i4cFsYeCq616GNGH7tk5c2xKdvAg&usqp=CAU',
+  ),
+  Img(
+    id: 6,
+    name: 'Crocodile',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZQssv32aKuZEnsEL3kjyrfe5_WqBRVLelGw&usqp=CAU',
+  ),
+  Img(
+    id: 7,
+    name: 'Hippopotamus',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpD4aaGCNbbjWulylh-lBDn38h9zY0XylPJA&usqp=CAU',
+  ),
+  Img(
+    id: 8,
+    name: 'Butterfly',
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBlTFT8mgoj9B8MkK5csSoryazMiQuLQmrQ&usqp=CAU',
+  ),
+];
